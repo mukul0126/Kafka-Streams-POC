2295cd8cec4087587cf6d84687bd034641fed4ab
Mukul Gupta <mukulgupta@Mukuls-MacBook-Pro.local>
2023-12-08 17:13:14 +0530

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,18 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 @SpringBootApplication
 public class Main {
-  public static void main(String[] args) {
+    public Main() throws IOException {
+    }
+
+    public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
   }
+  FileInputStream inputStream = new FileInputStream("test.txt");
+   FileInputStream c = null;
 }
